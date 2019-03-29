986cf613bb0ff2015033742da5e2fc6752405ecf
Elvis Ibarra <ielvisd@gmail.com>
2019-03-29 09:18:44 -0700

    opens a dir

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -7,12 +7,30 @@
 int main(int argc, char **argv)
 {
   // Parse command line
+  int i;
 
-  // Open directory
+  printf("There are %d command line argument(s):\n", argc);
 
-  // Repeatly read and print entries
+  for (i = 0; i < argc; i++)
+  {
+    printf("   %s\n", argv[i]);
+    // Open directory
+    DIR *dir = opendir(argv[i]);
+    struct dirent *entry;
 
-  // Close directory
+    if ((dir = opendir("/")) == NULL)
+      perror("opendir() error");
+    else
+    {
+      puts("contents of root:");
+      // Repeatly read and print entries
+      while ((entry = readdir(dir)) != NULL)
+        printf("  %s\n", entry->d_name);
+
+      // Close directory
+      closedir(dir);
+    }
+  }
 
   return 0;
 }
